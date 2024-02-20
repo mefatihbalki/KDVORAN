4ec0e093720bebbb7178ab07c748b6b6984962fe
Mehmet Fatih Balkı <159441317+mefatihbalki@users.noreply.github.com>
2024-02-20 12:20:29 +0300

    KDV oran

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+                Scanner input= new Scanner (System.in);
+                        System.out.println("Tutar Giriniz : ");
+                double tutar=input.nextDouble();
+
+                if (tutar>0 && tutar<1000) {
+                    double kdv1 = tutar * 0.18;
+                    System.out.println("Kdv1: " + kdv1);
+                    System.out.println("Toplam Tutar: " + (kdv1 + tutar));
+
+                }
+                else if (tutar>=1000){
+                        double kdv2= tutar*0.08;
+                        System.out.println("Kdv2: " + kdv2);
+                        System.out.println("Toplam Tutar: " + (kdv2+tutar));
+                    }
+                }
+    }
