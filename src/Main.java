b6138da6f4f89db1965eb4513c0819e76a852845
Mehmet Fatih Balkı <159441317+mefatihbalki@users.noreply.github.com>
2024-02-20 12:22:36 +0300

    yorum eklendi

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,12 @@ public class Main {
                 Scanner input= new Scanner (System.in);
                         System.out.println("Tutar Giriniz : ");
                 double tutar=input.nextDouble();
-
+//0 ile 1000 arasındaki tutar için
                 if (tutar>0 && tutar<1000) {
                     double kdv1 = tutar * 0.18;
                     System.out.println("Kdv1: " + kdv1);
                     System.out.println("Toplam Tutar: " + (kdv1 + tutar));
-
+//1000 ve yukarısında girilen tutarlar için
                 }
                 else if (tutar>=1000){
                         double kdv2= tutar*0.08;
